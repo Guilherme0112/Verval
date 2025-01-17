b388b077cdb0a169396c401678e34a0c2f4f65d2
Guilherme0112 <guimendesmen124@gmail.com>
2025-01-17 12:08:38 -0300

    tela de login

--- frontend/src/app/auth/login/page.js
+++ frontend/src/app/auth/login/page.js
@@ -1,5 +1,7 @@
 "use client"
 
+import style from "@/styles/Login.module.css";
+
 export default function Login(){
 
     async function onSubmit(event){
@@ -14,16 +16,21 @@ export default function Login(){
         const data = await response.json();
     }
     return (
-        <form onSubmit={onSubmit}>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" required />
-            </div>
-            <div>
-                <label htmlFor="password">Senha</label>
-                <input type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Entrar</button>
-        </form>
+        <main className="w-full h-screen flex justify-center items-center">
+            <form onSubmit={onSubmit} className={style.form}>
+                <div>
+                    <label htmlFor="email" className={style.label}>Email</label>
+                    <input type="email" id="email" className={style.input} name="email" required />
+                    <span className={style.underline}></span>
+                </div>
+                <div>
+                    <label htmlFor="password" className={style.label}>Senha</label>
+                    <input type="password" id="password" className={style.input} name="password" required />
+                    <span className={style.underline}></span>
+                </div>
+                <a href="" className="w-full pl-9">Esqueci minha senha</a>
+                <button type="submit">Entrar</button>
+            </form>
+        </main>
     );
 }
